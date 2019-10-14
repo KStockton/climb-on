b697ddb4989bc075602213ebffa261dc60b9b717
Michael KS <kingstockton@gmail.com>
2019-10-14 16:46:44 -0600

    lint and file structure

---
+++ src/components/Places/Places.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Sidebar from '../SideBar/SideBar.js';
+import Place from '../Place/Place.js';
+
+import './_Places.scss';
+
+class Places extends Component {
+
+  render() {
+    return (
+      <section className="Places" id="Places">
+        <h2 className="Places-header">Results</h2>
+        <Sidebar updateFilters={this.props.updateFilters}/>
+        <section className="Places-container">
+          {
+            this.props.places.map((currentPlace, i) => {
+              return ( 
+                <Place
+                  key={i}
+                  place={currentPlace.place}
+                  closestTown={currentPlace.closestTown}
+                  climbingId={currentPlace.climbingId}
+                  routes={currentPlace.routes}
+                />
+              );
+            })
+          }
+        </section>
+      </section>
+    );
+  }
+}
+
+
+export default Places;
